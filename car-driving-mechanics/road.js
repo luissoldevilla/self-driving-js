359f7da321731cb50bed5bcd8155dd6a40c7e429
luissoldevilla <luis.angelsoto@hotmail.com>
2022-10-12 12:35:54 -0400

    Road class: setting x and y axis pt2

--- car-driving-mechanics/road.js
+++ car-driving-mechanics/road.js
@@ -9,6 +9,13 @@ class Road {
 
         const infinity=1000000;
         this.top=-infinity;
-        this.bottom=infinity;
+        this.bottom=infinity;   
+
+        const topLeft={x:this.left,y:this.top};
+        const topRight={x:this.right,y:this.top};
+        const bottomLeft={x:this.left,y:this.bottom};
+        const bottomRight={x:this.right,y:this.bottom};
+
+        
     }
 }
