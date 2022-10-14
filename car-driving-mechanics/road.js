e44e678ec2f31400483d64bf7023379b13cd66c9
luissoldevilla <luis.angelsoto@hotmail.com>
2022-10-14 13:36:47 -0400

    car control.js: Function Draw pt5

--- car-driving-mechanics/road.js
+++ car-driving-mechanics/road.js
@@ -41,13 +41,17 @@ class Road {
                 this.right,
                 i/this.laneCount
             );
-            
+
             ctx.setLineDash([20,20]);
             ctx.beginPath();
             ctx.moveTo(x,this.top);
             ctx.lineTo(x,this.bottom);
             ctx.stroke();
-        }
 
+        }
+        ctx.setLineDash([]);
+        this.borders.forEach(border=>{
+            
+        })
     }
 }
